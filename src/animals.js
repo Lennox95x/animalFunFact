1c1dfcec33c4870958373137b1d2fd46670c2cf3
Lennox95x <lennart.janssenweets@googlemail.com>
2023-10-04 13:50:25 +0200

    Added the styles.css

---
+++ src/animals.js
@@ -0,0 +1,14 @@
+export const animals = {
+    staff: {
+      image: '/images/staff.jpg',
+      facts: ['American Staffordshire Terrier sind bekannt für ihre ausgezeichnete Zähigkeit und Ausdauer. Sie haben eine bemerkenswerte Muskelmasse und sind äußerst kräftig.', 'Obwohl sie ein Ruf als gefährliche Hunde haben, sind American Staffordshire Terrier tatsächlich liebevolle und treue Familienhunde. Sie sind bekannt für ihre freundliche und liebevolle Natur gegenüber Kindern.', 'Diese Hunde haben ursprünglich als Arbeits- und Farmhunde gedient. Sie wurden zur Jagd, zum Hüten von Vieh und als Wachhunde eingesetzt.']
+    },
+    lobster: {
+      image: '/images/lab.jpg',
+      facts: ['Lobsters taste with their legs!', 'Lobsters chew with their stomachs!', 'Lobsters can live as long as 100 years.']
+    },
+    starfish: {
+      image: '/images/cat.jpg',
+      facts: ['Starfish can have up to 40 arms!', 'Starfish have no brain and no blood!', 'Starfish can regenerate their own arms!']
+    }
+  };
